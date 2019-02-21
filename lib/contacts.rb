7705d32e15bfe2bef33e589e6c91c58a22ef53ee
Toni-Ty <antonique2479@yahoo.com>
2019-02-21 05:05:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,3 +19,9 @@ def remove_strawberry(contacts)
   binding.pry
   end
 end
+
+contacts.each do |person, contact_details_hash|
+  contact_details_hash.each do |attribute, data|
+    binding.pry
+  end
+end
